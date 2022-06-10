c94ec9f2b954ed68eb77b97d68252efe627d69e2
Guilherme <guilhermeleite.vieira91@gmail.com>
2022-06-09 23:40:57 -0300

    Estudo sobre Arrays

---
+++ Javascript-Youtube/listas.js
@@ -0,0 +1,44 @@
+const names = ["Felipe", "João", "Julia", 10, false];
+
+
+/* console.log(names[1]);
+
+names.push("Pedro");// adiciona ao final
+
+console.log(names);
+
+names.unshift("Guilherme")//adiciona no inicio
+
+console.log(names);
+
+names.pop();// elimina o ultimo
+
+console.log(names);
+
+names.shift();//elimina o primeiro
+
+console.log(names); */
+
+/* names.pop();
+names.pop();
+
+console.log(names); */
+
+
+/* console.log(names.indexOf("Julia")) */ 
+
+/* console.log(Array.isArray(names)); */
+
+let cores = ["Roxo","Vermelho","Azul","Laranja"]
+
+/* console.log(cores.indexOf("Laranja")) */
+
+/* console.log(cores.lastindexOf("Laranja")) // de tras pra frente
+ */
+
+const numbers = [1,2,3,4,5]
+
+/* const numbersMultipliedByTwo = numbers.map(function(i){ return i * 2;});
+
+console.log(numbersMultipliedByTwo) */
+
