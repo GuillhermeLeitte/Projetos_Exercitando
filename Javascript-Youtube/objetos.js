79d2fc1077306aa54118681c8279a6e6d1b32aef
Guilherme <guilhermeleite.vieira91@gmail.com>
2022-06-14 21:09:12 -0300

    Adicionei itens no arquivo objetos

--- Javascript-Youtube/objetos.js
+++ Javascript-Youtube/objetos.js
@@ -3,6 +3,10 @@ const person = {
     lastName: "Vieira Leite",
     age: 30,
     hobbies: ["Assistir jogo de futebol", "Assistir com minha esposa", "Ler - Estudar"],
+    dog: {
+        name: "Simba",
+        age: 4,
+    },
 
 };
 
@@ -15,9 +19,49 @@ const hobbies = person.hobbies; */
 //console.log(person.hobbies[1])
 
 /* const { firstName:primeiroNome, lastName, age,  hobbies } = person;
- */
+  */// Aqui vai colocar cada propriedade como variável.
+
 /* console.log(primeiroNome)
  */
+
+// person.dog = "Simba" // essa é uma forma de adicionar uma nova propriedade
+
+// Tambem posso adicionar um objeto dentro do meu objeto. Adicionei direto no meu obejto um outro objeto.
+
+// Como tornar cada propriedade do objeto dog que esta dentro do objeto person, em variável?
+
+/* const {name, age} = person.dog
+console.log(name, age) */ // Aqui eu transformei cada propriedade do objeto dog em variável
+
+const todos = [
+    {
+        id: 1,
+        description: "Estudar programação",
+        isCompleted: false,
+    },
+    {
+        id: 2,
+        description: "Ler",
+        isCompleted: true,
+    },
+    {
+        id: 3,
+        description: "Treinar",
+        isCompleted: true,
+    },
+];
+
+//Como acessar o ultimo obejto dentro dessa lista acima?
+
+// console.log(todos[2].description)
+// ou
+// const descriptionId = todos[2].description
+
+
+
+
+
+
 
 
 
