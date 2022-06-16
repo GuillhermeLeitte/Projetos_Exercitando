23854dfdb7787f97bb86894d26b456b36f0e27f2
Guilherme <guilhermeleite.vieira91@gmail.com>
2022-06-16 19:55:40 -0300

    Adicionei o arquivo funcoes

---
+++ Javascript-Youtube/funcoes.js
@@ -0,0 +1,17 @@
+// function sum(a, b= 10){
+//     return a + b;
+// }
+//  const sumVale = sum(2)
+
+//  console.log(sumVale)
+
+// Arrow function
+
+// const sumArrow = (a, b) => {
+//     return a + b; 
+// }
+
+// console.log(sumArrow(2,4))
+
+
+
