4291a90a932ace5435626236df9667f21728ba00
Guilherme <guilhermeleite.vieira91@gmail.com>
2022-06-21 21:10:53 -0300

    Outra resolução

--- Javascript-Youtube/máx-entre-dois.js
+++ Javascript-Youtube/máx-entre-dois.js
@@ -13,3 +13,16 @@ function maiorNum (num1 = 0, num2 = 0){
 console.log(maiorNum());
 console.log(maiorNum(2,3));
 console.log(maiorNum(1,2));
+
+// Outra forma de fazer de fazer
+
+function máx (numero1, numero2){
+    if (numero1 > numero2){
+        return numero1
+    } else {
+        return numero2
+    }
+}
+
+let valorMaior = máx(13,13)
+console.log(valorMaior)
